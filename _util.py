5dca45c08240382c2a14c8138ea2bd4059682dbc
alextdnreb <alex.berndt95@web.de>
2024-11-23 16:37:22 +1300

    remove deprecated numpy use

--- _util.py
+++ _util.py
@@ -4,7 +4,7 @@ import numpy as np
 def to_bytes(x: int, length: int) -> bytes:
     x = [None, np.uint8, np.uint16, None, np.uint32][length](x)
     # func copied from https://github.com/Gael-de-Sailly/geo-mapgen/blob/4bacbe902e7c0283a24ee3efa35c283ad592e81c/database.py#L34
-    res = x.newbyteorder("<").tobytes()
+    res = x.view(x.dtype.newbyteorder("<")).tobytes()
     assert len(res) == length
     return res
 
