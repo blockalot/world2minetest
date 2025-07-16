e58683703ff898d707a157d2818e9ae35faff29d
alextdnreb <alex.berndt95@web.de>
2025-07-16 22:45:17 +0200

    adjust assert with if

--- generate_map.py
+++ generate_map.py
@@ -263,8 +263,8 @@ else:
             a[yyy, xxx, 2] = np.minimum(255, 127 + ground_z + 1)
         if height is not None and building.get("is_part"):
             # only overwrite height if it is likely from the same building
-            assert height >= 1
-            a[yyy, xxx, 3] = np.minimum(255,ground_z + height + 127)
+            if height >= 1:
+                a[yyy, xxx, 3] = np.minimum(255,ground_z + height + 127)
 
         else:
             a[yyy, xxx, 3] = np.maximum(a[yyy, xxx, 3], np.minimum(255,ground_z + (height or 1) + 127))
